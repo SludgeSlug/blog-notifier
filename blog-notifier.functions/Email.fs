97a290eae06903d939e71f3745b45e4bae34173a
SludgeSlug <philip.wheldon@googlemail.com>
2014-04-13 15:12:02 +0100

    :volcano: Adding function for sending emails

---
+++ blog-notifier.functions/Email.fs
@@ -0,0 +1,14 @@
+﻿module Email
+
+    open System.Net.Mail
+
+    let sendEmail = 
+        let msg = new MailMessage("blog-notifier@test.com",
+                                  "blog-notifier@mailinator.com",
+                                  "Test email subject",
+                                  "Test email body")
+
+        let client = new SmtpClient(@"smtp.mailinator.com")
+        client.DeliveryMethod <- SmtpDeliveryMethod.Network
+
+        client.Send(msg)
