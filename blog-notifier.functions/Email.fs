28913ee06f1b094dc9b511db0dfcc88aa70bafcc
SludgeSlug <philip.wheldon@googlemail.com>
2014-04-13 22:02:06 +0100

    :crown: Content of email is generated by querying a URL

--- blog-notifier.functions/Email.fs
+++ blog-notifier.functions/Email.fs
@@ -1,12 +1,24 @@
 ﻿module Email
 
     open System.Net.Mail
+    open System.Net
+    open System.IO
+    
+    let getXmlFromBlog =
+        let url = "http://blogs.nhs.uk/choices-blog/feed/"
+
+        let req = WebRequest.Create(url)
+        req.Method <- "GET"
+
+        let reader = new StreamReader(req.GetResponse().GetResponseStream())
+
+        reader.ReadToEnd().ToString()
 
     let sendEmail = 
         let msg = new MailMessage("blog-notifier@test.com",
                                   "blog-notifier@mailinator.com",
                                   "Test email subject",
-                                  "Test email body")
+                                  getXmlFromBlog)
 
         let client = new SmtpClient(@"smtp.mailinator.com")
         client.DeliveryMethod <- SmtpDeliveryMethod.Network
